992d0964e55b730f492624bcf518f1fdec042b5d
rjames <rjames@rjames-mbpr-us.lan>
2022-06-18 07:44:53 -0400

    changed function

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,11 +1,27 @@
+const { url } = require('inspector');
+
 exports.handler = async function (event) {
     const form = JSON.parse(event.body).payload.data;
     console.log(form)
     const fs = require('fs');
-    const data = form.url + ' ' + form.route;
+    const data = [];
 
-    fs.writeFile('../../_redirects', data, (err) => {
-        if (err) throw err;
+    for(let item in form) {
+        let destination = form.url;
+        if(destination.indexOf("://") === -1) {
+            destination = "https://" + destination;
+        }
+        data.push("/" + url.route + "  " + destination + "  302");
+    }
+
+    
+
+    fs.writeFile(form.referrer + '/_redirects', data.join('\n'), function(err) {
+        if(err) {
+            return console.log(err);
+        } else {
+            return console.log('New routes saved.')
+        }
     });
 
     return {
